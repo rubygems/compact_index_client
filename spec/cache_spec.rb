8f9af50a7727ede27b09eea045bba4678dc0d9c6
Samuel E. Giddins <segiddins@segiddins.me>
2015-08-23 00:07:21 -0700

    Add spec skeleton

--- spec/cache_spec.rb
+++ spec/cache_spec.rb
@@ -1,4 +1,65 @@
 require "spec_helper"
 
 describe CompactIndexClient::Cache do
+  subject(:cache) { described_class.new(directory) }
+
+  describe "#initialize" do
+    it "has a directory" do
+      expect(cache.directory).to eq(directory)
+    end
+
+    it "creates the directory if it doesn't exist" do
+      directory.rmtree if directory.directory?
+      cache
+      expect(directory).to be_a_directory
+    end
+
+    it "doesnt delete the directory if it exists" do
+      directory.mkpath
+      directory.+("foo").write("")
+      cache
+      expect(directory.+("foo")).to be_a_file
+    end
+  end
+
+  describe "paths" do
+    it "has a names path" do
+      expect(cache.names_path).to eq(directory + "names")
+    end
+
+    it "has a versions path" do
+      expect(cache.versions_path).to eq(directory + "versions")
+    end
+
+    it "has info paths" do
+      expect(cache.info_path("foo")).to eq(directory + "info/foo")
+    end
+  end
+
+  def write(path, contents)
+    path.open("w") {|f| f << contents }
+  end
+
+  describe "#names" do
+    let(:names) { %w(foo bar baz quz) }
+
+    it "parses out names" do
+      write(cache.names_path, names.join("\n"))
+      expect(cache.names).to eq(names)
+    end
+
+    it "ignores a leading comment" do
+      write(cache.names_path, "foo\n---\n" << names.join("\n"))
+      expect(cache.names).to eq(names)
+    end
+  end
+
+  describe "#versions" do
+  end
+
+  describe "#dependencies" do
+  end
+
+  describe "#specific_dependency" do
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,17 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "compact_index_client"
+require "tmpdir"
+
+RSpec.configure do |c|
+  def directory
+    @directory
+  end
+
+  c.before(:each) do
+    @directory = Pathname.new(Dir.mktmpdir)
+  end
+
+  c.after(:each) do
+    directory.rmtree if directory.directory?
+  end
+end
